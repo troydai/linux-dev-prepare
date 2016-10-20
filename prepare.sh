cebec4b4d6d7ab99579b7856abb792ea635ffeb5
Troy Dai <troy.dai@outlook.com>
2016-10-20 10:06:30 -0700

    update Thu Oct 20 10:06:30 PDT 2016

--- prepare.sh
+++ prepare.sh
@@ -7,3 +7,11 @@ else
     echo "Missing /etc/os-release, which is used to determine underlying OS"
     exit 1
 fi
+
+if [ "$osid" == "centos" ]; then
+    feed=https://dotnetcli.blob.core.windows.net/dotnet/preview/Binaries/1.0.0-preview2.1-003155/dotnet-dev-centos-x64.1.0.0-preview2.1-003155.tar.gz
+    sudo yum install libunwind libicu
+    curl -sSL -o dotnet.tar.gz $feed
+    sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+    sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+fi
