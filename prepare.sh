0b2421e07e8b9b61967b64d06d90286779125311
Troy Dai <troy.dai@outlook.com>
2016-10-20 09:53:21 -0700

    update

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Test test test!"
+if [ -e /etc/os-release ]; then
+    osid=$(cat /etc/os-release | grep ^ID= | cut -d= -f2 | tr -a '"')
+    echo "Pave OS $osid"
+else
+    echo "Missing /etc/os-release, which is used to determine underlying OS"
+    exit 1
+fi
