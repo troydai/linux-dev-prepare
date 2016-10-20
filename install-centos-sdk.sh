d7e89bef8e071649af07bc91e9fd61d5daaadd2c
Troy Dai <troy.dai@outlook.com>
2016-10-20 11:32:55 -0700

    update

--- install-centos-sdk.sh
+++ install-centos-sdk.sh
@@ -10,9 +10,7 @@ sudo yum -y update
 sudo yum -y install libunwind libicu
 
 if [ "$sdk_version" == "1.0.1" ]; then
-    curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=827529
-    sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
-    sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+    url=https://go.microsoft.com/fwlink/?LinkID=827529
 elif [ "$sdk_version" == "1.1.0" ]; then
     ver=1.0.0-preview2.1-003155
     url=https://dotnetcli.blob.core.windows.net/dotnet/preview/Binaries/$ver/dotnet-dev-$os-$arch.$ver.tar.gz
@@ -21,6 +19,7 @@ else
     exit 1
 fi
 
+curl -sSL -o dotnet.tar.gz $url
 sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
 sudo ln -s /opt/dotnet/dotnet /usr/local/bin
 rm dotnet.tar.gz
